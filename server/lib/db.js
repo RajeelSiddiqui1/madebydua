59783f4fc9acc73b6afc72061b7ab1c238416ce6
Rajeel Siddiqui github <159783237+RajeelSiddiqui1@users.noreply.github.com>
2026-03-25 01:59:03 +0500

    set db.js file

--- server/lib/db.js
+++ server/lib/db.js
@@ -3,9 +3,19 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.log("MongoDB connection error:", err));
+const MONGODB_URI = process.env.MONGODB_URI;
+
+const dbConnect = async () => {
+  try {
+    await mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`mongodb connect successfully ${MONGODB_URI}`);
+  } catch (error) {
+    console.log(`mongodb not connect successfully ${error}`);
+  }
+};
+
+export default dbConnect;
+
