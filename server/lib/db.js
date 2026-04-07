72f05536262af9354e65628b5d8ea07addaaa362
Rajeel Siddiqui github <159783237+RajeelSiddiqui1@users.noreply.github.com>
2026-04-08 02:27:23 +0500

    set .env

--- server/lib/db.js
+++ server/lib/db.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const MONGODB_URI = process.env.MONGODB_URI_LOCAL || process.env.MONGODB_URI_LOCAL;
+const MONGODB_URI = process.env.MONGODB_URI || process.env.MONGODB_URI_LOCAL;
 
 const dbConnect = async () => {
   try {
